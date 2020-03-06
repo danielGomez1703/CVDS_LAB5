e885a6d6d18727cafe87246dee99fd566d233739
daniel gomez <danipipe1703@gmai.com>
2020-03-06 17:39:18 -0500

    test

---
+++ Game/edu.eci.cvds/src/test/java/edu/eci/cvds/guessGame/GuessGameTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cvds.guessGame;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author daniel.gomez-su
+ */
+public class GuessGameTest {
+    
+    public GuessGameTest() {
+    }
+}
